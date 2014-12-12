0f252d4a18ba92418d28966e2599132d636d0cc7
dbarobin <dbarobinwen@gmail.com>
2014-12-12 18:17:19 +0800

    add auto_deploy_app_to_tomcat/auto_scp_tomcat_log.sh scripts

--- auto_deploy_app_to_tomcat/auto_scp_tomcat_log.sh
+++ auto_deploy_app_to_tomcat/auto_scp_tomcat_log.sh
@@ -5,20 +5,27 @@
 # Desc: Auto scp tomcat logs.
 
 # Copy the tomcat logs.
+
+# Yesterdate date. Fommat 2014-12-12.
 date_yesterday=`date -d yesterday +%F`
+# Today date. Fommat 2014-12-12.
 date_today=`date +%F`
+# Tomorrow date. Fommat 2014-12-12.
 date_tomorrow=`date -d next-day +%F`
 
+# Copy the yesterday's tomcat log.
 if [ ! -f "localhost.$date_yesterday.log" ]
 then
 	scp YOUR_NAME@YOUR_IP:YOUR_TOMCAT_HOME/logs/localhost.$date_yesterday.log YOUR_PATH/logs/tomcat 2>/dev/null
 fi
 
+# Copy the today's tomcat log.
 if [ ! -f "localhost.$date_today.log" ]
 then
 	scp YOUR_NAME@YOUR_IP:YOUR_TOMCAT_HOME/logs/localhost.$date_today.log YOUR_PATH/logs/tomcat 2>/dev/null
 fi
 
+# Copy the tomorrow's tomcat log.
 if [ ! -f "localhost.$date_tomorrow.log" ]
 then
 	scp YOUR_NAME@YOUR_IP:YOUR_TOMCAT_HOME/logs/localhost.$date_tomorrow.log YOUR_PATH/logs/tomcat 2>/dev/null
