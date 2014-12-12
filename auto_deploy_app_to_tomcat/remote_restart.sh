024f96d308842fe33d35e93476156c8555983b2d
dbarobin <dbarobinwen@gmail.com>
2014-12-12 18:31:49 +0800

    update the remote_restartup.sh script

--- auto_deploy_app_to_tomcat/remote_restart.sh
+++ auto_deploy_app_to_tomcat/remote_restart.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Java Home.
 export JAVA_HOME=YOUR_JAVA_HOME
+# CATALINA Home.
 export CATALINA_HOME=YOUR_CATALINA_HOME
 
+# Execute the shutdown.sh scripts.
 YOUR_JAVA_HOME/bin/shutdown.sh
+# Execute the startup.sh scripts.
 YOUR_JAVA_HOME/bin/startup.sh
